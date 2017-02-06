8dba7c381a6fe8e45495b8eea8cd7a74b47ba8cb
JeremieLetonnelier <jereleto@gmail.com>
2017-02-06 16:43:09 +0100

    Ajout test serveur

--- sensors/test_serveur.py
+++ sensors/test_serveur.py
@@ -6,12 +6,12 @@ s3_client = boto3.client('s3')
 s3 = boto3.resource('s3')
 #On se connecte au serveur S3
 
-for bucket in s3.buckets.all():
+'''for bucket in s3.buckets.all():
         print(bucket.name);
 
 my_session = boto3.session.Session()
 my_region = my_session.region_name
-print(my_session)
+print(my_session)'''
 #chgt de région pour le bucket
 #pb avec la région passage en eu-central cf le fichier texte
 #s3.meta.client.upload_file('/Users/jerel/Documents/Cours/2A/Projets/saintgobain/IoT_Mines/sensors/hello.txt', 'sensortag', 'hello.txt')
