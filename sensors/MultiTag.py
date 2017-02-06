64a0b32b259f08b2f00c22365befef90fac06078
paguilmin <pauline.guilmin2@etu.univ-lorraine.fr>
2017-02-06 16:27:38 +0000

    maj script multitag.py

--- sensors/MultiTag.py
+++ sensors/MultiTag.py
@@ -9,15 +9,22 @@ from bluepy import sensortag
 import os,sys
 import argparse
 from time import gmtime, strftime, sleep
+import datetime
 
 import numpy as np
 import requests,json
+import boto3
+from botocore.client import Config
+from botocore.exceptions import ClientError
+
+from gpio import gpio
 
 def autopairing(timeout=10.0,devices=None):
     '''Autoscan BLE for neighbouring advertising Sensortag and turn on environmental sensors
     '''
     if devices is None: # Auto scan if no sensors are provided
         BLE=Scanner()
+
         print "scanning in progress \n"
         devices = BLE.scan(timeout)
 
@@ -56,66 +63,60 @@ def autopairing(timeout=10.0,devices=None):
 
 def acquire(Sensors):
     '''Read sensors measured value and return a data dictionary
-
     Warning : this is not error proof : any disconnection from one of the sensor make the full function to crash.
     '''
 
-    data={}
-    data['LocalTime']=gmtime()
+    data=[]
     for tag in Sensors:
         meas={}
-        loc=tag.IRtemperature.read()
-        meas['IRamb']=loc[0] # IR Temperature in degC
-        meas['IRobj']=loc[1] # IR Temperature in degC
+        meas['ID']=tag.addr
         loc=tag.humidity.read()
-        meas['T']=loc[0] # T in degC
-        meas['RH']=loc[1] #HR in %
-        meas['Illum']=tag.lightmeter.read() # Illuminance
+        meas['Temperature']=loc[0] # T in degC
+        meas['Humidite']=loc[1] #HR in %
+        meas['Luminosite']=tag.lightmeter.read() # Illuminance
+        meas['creation_datetime']=datetime.datetime.now().strftime("%Y_%m_%d-%H:%M:%S UTC")
         # Add relevant measurement here.
-
         # Adding the measured value to the globale dict.
-        data[tag.addr]=meas
-
+        data.append(meas)
     return data
 
 
-
-def sendJson(data,session=None):
+def sendJson_TI(input_json):
     """send JSON to Amazon bucket """
-    if not session:
-        session=requests.Session() # keep session alive
-        session.headers = {'Content-Type': 'application/json','Accept': 'application/json'} # typical, modify to add credentials if needed
-        session.proxies={} # Add proxies if needed
-        session.url='' # input address of the server here
-        last_data=open('unsent_data.json', 'w') # create back-up file
-        last_data.write('')
-        last_data.close()
 
+    s3_client = boto3.client('s3')
+    s3 = boto3.resource('s3')
+    current_time=gmtime()
+
+    filename='TI'+ datetime.datetime.now().strftime("%Y_%m_%d-%H:%M:%S UTC")
+
+#    filename='/home/pi/data/'+filename
     try:
-        with open('unsent_data.json', 'a+') as last_data:
-            last_data.write(json.dumps(data)+'\r\n') # save data in case of something bad happen
-
-        with open('unsent_data.json', 'a+') as last_data:
-            data_=last_data.readlines()
-
-
-
-        for text in data_:
-            res=self._session.post(self.url,data=text) # adapt to fit your server API ! (this is for Amazon S3 server)
-
-            if res.status_code != 200:
-                common.display('HTTP : ' +res.content)
-                break
-        if res.status_code == 200:
-            common.display('data sent to server')
-            last_data=open('unsent_data.json', 'w')
-            last_data.write('')
-            last_data.close()
+        with open(filename+'.txt', 'w+') as doc:
+            doc.write(json.dumps(input_json))
+        s3_client.upload_file(filename+'.txt', 'sensortag',filename+'.txt')
 
     except Exception as e:
-        print('HTTP : ' + str(e.message)) # not crashing if error
-    finally:
-        return session
+        print('BOTO : ' + str(e.message)) # not crashing if error
+
+
+def sendJson_windows(input_json):
+    """send JSON to Amazon bucket """
+
+    s3_client = boto3.client('s3')
+    s3 = boto3.resource('s3')
+    current_time=gmtime()
+
+    filename='windows'+ datetime.datetime.now().strftime("%Y_%m_%d-%H:%M:%S UTC")
+#    filename='/home/pi/data/'+filename
+    try:
+        with open(filename+'.txt', 'w+') as doc:
+            doc.write(json.dumps(input_json))
+        s3_client.upload_file(filename+'.txt', 'sensortag',filename+'.txt')
+
+    except Exception as e:
+        print('BOTO : ' + str(e.message)) # not crashing if error
+
 
 def dispData(verbose,data=None):
     ''' display data if verbose is True and data are provided'''
@@ -123,14 +124,44 @@ def dispData(verbose,data=None):
         return
     else:
         first=True
-        for k in data.keys():
-            if not 'LocalTime' in k:
+        for meas in data:
                 if first :
-                    print  strftime("%d %b %Y %H:%M:%S", data['LocalTime'])+'  :  ' + str(data[k].keys())
+                    print  datetime.datetime.now().strftime("%Y_%m_%d-%H:%M:%S UTC")+'  :  ' + str(meas.keys())
                     first=False
-                print k+ '  :  '+str([data[k][k2] for k2 in data[k].keys()])
+                print ' :'+str([meas[k2] for k2 in meas.keys()])
 
-def main():
+
+"""
+def sendJson_error(input_json):
+    #send an error JSON to Amazon bucket
+
+    s3_client = boto3.client('s3')
+    s3 = boto3.resource('s3')
+    current_time=gmtime()
+    filename=datetime.datetime.now().strftime("%Y_%m_%d-%H:%M:%S UTC")
+#    filename='/home/pi/data/'+filename
+    try:
+        with open(filename+'.txt', 'w+') as doc:
+            doc.write(json.dumps(input_json))
+        s3_client.upload_file(filename+'.txt', 'sensortagerror',filename+'.txt')
+    except Exception as e:
+        print('BOTO : ' + str(e.message)) # not crashing if error
+
+"""
+
+def dispData(verbose,data=None):
+    ''' display data if verbose is True and data are provided'''
+    if not verbose or data is None:
+        return
+    else:
+        first=True
+        for meas in data:
+                if first :
+                    print  datetime.datetime.now().strftime("%Y_%m_%d-%H:%M:%S UTC")+'  :  ' + str(meas.keys())
+                    first=False
+                print ' :'+str([meas[k2] for k2 in meas.keys()])
+
+def old_main():
     if not os.geteuid() == 0:
         sys.exit('Script must be run as root')
     parser = argparse.ArgumentParser()
@@ -154,8 +185,26 @@ def main():
         sleep(arg.t)
 
 
-
-
+def main():
+    print "Init"
+    sensors=autopairing()
+    print "Done"
+    print "GPIO Init"
+    #fixme : update pin number
+    windows_dict={}
+    windows=[gpio(12,'Tableau'), gpio(21,'Milieu'), gpio(17,'Fond')]
+    for w in windows:
+        windows_dict[w.ID] = w.getState()
+    windows_dict[creation_datetime] = datetime.datetime.now().strftime("%Y_%m_%d-%H:%M:%S UTC")
+    # main loop / no error managment
+    print "Starting measurements"
+    while True:
+        data=acquire(sensors)
+        for i in range (0,len(data)+1):
+            sendJson_TI(data[i])
+        sendJson_windows(windows_dict)
+        dispData(True,data)
+        sleep(300) # Prise de mesure toutes les 5 minutes environ
 
 
 if __name__ == '__main__':
