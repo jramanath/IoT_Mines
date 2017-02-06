0d37b64bb9277a93830c9dea37caad89572b5a76
Pauline GUILMIN <paguilmin@MacBook-Air-de-Pauline.local>
2017-02-06 16:54:04 +0100

    commentaire + squelette table DataCapteursTI

--- sensors_manager/data_collector/models.py
+++ sensors_manager/data_collector/models.py
@@ -13,14 +13,25 @@ class DataLoader(models.Model):
     Table contenant le nom des fichiers déjà collectés
     """
 
+    # à mettre pour chaque table
     id = models.AutoField(primary_key=True)
+    # on va y mettre l'identification du fichier json
     file = models.CharField(max_length=100, blank=True, null=True)
 
+    #va permettre l'analyse de données
     objects = DataFrameManager()
 
+    # convertit classe meta en data_frame (passer de queryset à column=[id, file])
+    #remplace DataLoader.objects.all().to.data_frame()
     class Meta(object):
         managed = True
         db_table = 'data_loader'
 
+class DataCapteursTI(models.Model):
+    """
+    Table contenant les données des capteurs
+    """
+
+    
 
 #  Créer le modèle des autres tables
