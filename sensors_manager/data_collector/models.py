52e7bcdb8653e59c29f2c07582501e94e718e101
Pauline GUILMIN <paguilmin@MacBook-Air-de-Pauline.local>
2017-02-14 12:47:30 +0100

    ajout des modèles capteursTI et capteursfenetres

--- sensors_manager/data_collector/models.py
+++ sensors_manager/data_collector/models.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django_pandas.managers import DataFrameManager
+from django.utils import timezone
 
 
 class DataLoader(models.Model):
@@ -27,11 +28,36 @@ class DataLoader(models.Model):
         managed = True
         db_table = 'data_loader'
 
+
+
 class DataCapteursTI(models.Model):
     """
     Table contenant les données des capteurs
     """
 
-    
+    id = models.AutoField(primary_key=True)
+    #date et heure
+    datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+    #temperature en degres celsius
+    Temperature = models.DecimalField(max_digits=4, decimal_places=2)
+    # humidite en pourcentage
+    RH = models.DecimalField(max_digits=4, decimal_places=2)
+    #luminosite
+    Illum = models.DecimalField(max_digits=6, decimal_places=2)
+    #code du capteur
+    ID = models.CharField(max_length=100, blank=True, null=True)
+
+class DataCapteursFenetres(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    #date et heure
+    datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
+    windows_state = models.BooleanField()
+
+
+
+
+
+
 
 #  Créer le modèle des autres tables
