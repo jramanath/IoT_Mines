654af02e1cf9cba606335ee986fac6b59a692528
jramanath <jacques.ramanathan@saint-gobain.com>
2017-03-21 19:01:59 +0100

    update template settings_env

--- sensors_manager/sensors_manager/settings_env.template.py
+++ sensors_manager/sensors_manager/settings_env.template.py
@@ -8,6 +8,7 @@ pour mettre notamment les codes d'accès à Amazon S3
 AWS_ACCESS_KEY_ID = ""  # TODO: remplacer par la bonne clé
 AWS_SECRET_ACCESS_KEY = ""  # TODO: remplacer par la bonne clé
 BUCKET_NAME = ""  # TODO: remplacer par le bon nom
+REGION_NAME = ""  # TODO: remplacer par la bonne région
 
 # où sont les logs
 LOGS_DIR = ''  # TODO: remplacer par le bon chemin
